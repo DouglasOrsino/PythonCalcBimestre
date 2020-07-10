f482803ea6fd5e302ea31919c2f4d92f0f95a255
Douglas Orsino <douglas.orsino123@outlook.com>
2020-07-09 21:58:05 -0300

    Programa para calcular a média em Python.

---
+++ calc_bimestre.py
@@ -0,0 +1,26 @@
+a = float(input('Primeiro bimestre: '))
+if a > 10:
+    a = float(input('Você digitou errado. Primeiro bimestre: '))
+
+b = float(input('Segundo bimestre: '))
+if b > 10:
+    b = float(input('Você digitou errado. Segundo bimestre: '))
+
+c = float(input('Terceiro bimestre: '))
+if c > 10:
+    c = float(input('Você digitou errado. Terceiro bimestre: '))
+
+d = float(input('Quarto bimestre: '))
+if d > 10:
+    d = float(input('Você digitou errado. Quarto bimestre: '))
+
+media = (a + b + c + d) / 4
+
+print('A média do aluno é: {}'.format(media))
+
+if media >= 6:
+    print('Aluno aprovado')
+else:
+    print('Está em recuperação')
+
+
